6f2906942031b48059a56db6b6de673055ca2025
sam <sammy627@gmail.com>
2021-10-11 20:47:34 -0500

    Update interface.js

--- assets/js/interface.js
+++ assets/js/interface.js
@@ -1,4 +1,4 @@
-var minersAddr = '0xBA960c22ba1de24CAAa32dF459330AC50d3308Fd';
+var minersAddr = '0x270Ff6601b5Deb8E8F8DccB4d0b69a2542AEf1F9';
 var tokenAddr = '0xe9e7cea3dedca5984780bafc599bd69add087d56';
 var minersAbi =
 [
